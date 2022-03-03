8a58adce466fc8bb350b3f7df18d6e7ab56d57a0
Lasse Skaalum <lsk@trifork.com>
2022-03-03 10:30:30 +0100

    Change to pointer types in output

--- c-neural-network/neuralNetwork.c
+++ c-neural-network/neuralNetwork.c
@@ -7,24 +7,24 @@
 
 #define TRAINING_TIME 1500
 
-void feedForward(Matrix x, Matrix weights1, Matrix weights2, Matrix output)
+void feedForward(Matrix x, Matrix weights1, Matrix weights2, Matrix *output)
 {
     Matrix firstLayerDotProductResult = mallocMatrix(4, 4);
-    matrixDotProduct(x, weights1, firstLayerDotProductResult);
+    matrixDotProduct(x, weights1, &firstLayerDotProductResult);
 
     Matrix sigmoidedMatrix = mallocMatrix(4, 4);
-    sigmoidMatrix(firstLayerDotProductResult, sigmoidedMatrix);
-    freeMatrix(firstLayerDotProductResult);
+    sigmoidMatrix(firstLayerDotProductResult, &sigmoidedMatrix);
+    freeMatrix(&firstLayerDotProductResult);
 
     Matrix outputLayerMatrix = mallocMatrix(4, 1);
-    matrixDotProduct(sigmoidedMatrix, weights2, outputLayerMatrix);
-    freeMatrix(sigmoidedMatrix);
+    matrixDotProduct(sigmoidedMatrix, weights2, &outputLayerMatrix);
+    freeMatrix(&sigmoidedMatrix);
 
     sigmoidMatrix(outputLayerMatrix, output);
-    freeMatrix(outputLayerMatrix);
+    freeMatrix(&outputLayerMatrix);
 }
 
-// void backPropagate(double **x, double **y)
+// void backPropagate(Matrix weights2, Matrix weights1, Matrix y, Matrix x, Matrix weights2Output, Matrix weights1Output)
 // {
 // }
 
@@ -47,6 +47,7 @@ void fillMatrixWithRandomDoubles(Matrix m)
 int main()
 {
     srand(42);
+
     // Initializing weights
     Matrix weights1 = mallocMatrix(3, 4);
     fillMatrixWithRandomDoubles(weights1);
@@ -71,20 +72,16 @@ int main()
     x.matrix[3][1] = 1;
     x.matrix[3][2] = 1;
 
-    // Initializing output values
-    // double y[ROWS][1] = {
-    //     {0},
-    //     {1},
-    //     {1},
-    //     {0}
-    // };
-
     Matrix output = mallocMatrix(4, 1);
-    feedForward(x, weights1, weights2, output);
-
+    feedForward(x, weights1, weights2, &output);
     printMatrix(output);
 
-    // sigmoidMatrix(matrixResult, ROWS, WEIGHT_COLUMNS);
+    // Initializing output values
+    Matrix y = mallocMatrix(4, 1);
+    y.matrix[0][0] = 0;
+    y.matrix[1][0] = 1;
+    y.matrix[2][0] = 1;
+    y.matrix[3][0] = 0;
 
     for (int i = 0; i < TRAINING_TIME; i++)
     {
