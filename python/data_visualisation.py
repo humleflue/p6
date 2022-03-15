fc1807cfd58344724506f9817fe7e5b20d66c723
Lasse Skaalum <lsk@trifork.com>
2022-03-15 09:05:57 +0100

    Reformat

--- python/data_visualisation.py
+++ python/data_visualisation.py
@@ -1,11 +1,11 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-import glob
 import os
 import data_api.retrieve_data as rd
 
 data_path = 'datasets'
 directories = ["Driving", "High vibration", "Walking", "Hand tools", "Industrial equipment", "Stationary"]
+
 
 def get_file_name_from_path(file):
     # Removes parent directories
@@ -13,12 +13,14 @@ def get_file_name_from_path(file):
     # Removes file extension
     without_type = with_type.split(".")[0]
     return without_type
-    
+
+
 def save_plot(filepath, filename):
     ax = pd.read_csv(filepath, sep=";", index_col=None, header=0)
     ax.plot(title=filename, kind='line')
     # Relative path from p6
-    plt.savefig(os.path.join('visualisations', 'plots',f'{filename}.png'), format='png')
+    plt.savefig(os.path.join('visualisations', 'plots', f'{filename}.png'), format='png')
+
 
 def save_all_plots(path, folder_name, sampling):
     all_files = rd.get_all_files_in_folder(path, folder_name, '*.csv')
@@ -28,9 +30,11 @@ def save_all_plots(path, folder_name, sampling):
         title = f'{filename} ({sampling})'
         save_plot(filepath, title)
 
+
 def main():
     for element in directories:
         save_all_plots(data_path, element, "None")
+
 
 if __name__ == '__main__':
     main()
