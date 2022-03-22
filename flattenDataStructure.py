f1e68d4c5f982901f026d2de35a48f45c20e9232
JacobToftgaardRasmussen <1jatora@gmail.com>
2022-03-22 09:07:38 +0100

    refactored into functions

--- flattenDataStructure.py
+++ flattenDataStructure.py
@@ -1,28 +1,35 @@
 import pandas as pd
 import numpy as np
 import math
+from os import listdir
+
 shortest_file_length = 768
 shortest_file_name = "Dataset air wrench 1.csv"
-number_of_measurements_for_time_series = 180 #15 seconds of measurements
+rows_per_time_series = 180 #15 seconds of measurements
 
-from os import listdir
-def load_dataset(dirName="datasets"):
+def flatten_datasets(dirName="datasets"):
     all_datasets_flattened = []
-    length_of_shortest_dataset = 1000000
-    name_of_shortest_dataset = "foo"
+    all_file_paths = create_all_file_paths(dirName)
+
+    for file in all_file_paths:
+        df = pd.read_csv(file, delimiter=";")
+        number_of_rows_in_df = df.shape[0]
+        for partition in range(math.floor(number_of_rows_in_df/rows_per_time_series)):
+            time_series_partition = create_time_series_partition(partition, rows_per_time_series, df, file)
+            all_datasets_flattened.append(time_series_partition)
+    return pd.DataFrame(all_datasets_flattened)
+
+def create_time_series_partition(partition, rows_per_time_series, dataFrame, label):
+    time_series = dataFrame.iloc[partition*rows_per_time_series:(partition + 1 )*rows_per_time_series,:]
+    time_series = time_series.unstack().to_frame().sort_index(level=1).T
+    time_series = time_series.assign(label=[label])
+    return time_series.values.tolist()[0]
+
+def create_all_file_paths(dirName="datasets"):
+    all_file_paths = []
     for subDir in listdir(dirName):
         for fileName in listdir(f"{dirName}/{subDir}"):
-            if(fileName == "Dataset air wrench 1.csv" or True):
-                path_to_file = f"{dirName}/{subDir}/{fileName}"
-                df = pd.read_csv(path_to_file, delimiter=";")
-                number_of_rows_in_df = df.shape[0]
-                for partition in range(math.floor(number_of_rows_in_df/number_of_measurements_for_time_series)):
-                    time_series = df.iloc[partition*number_of_measurements_for_time_series:(partition + 1 )*number_of_measurements_for_time_series,:]
-                    time_series = time_series.unstack().to_frame().sort_index(level=1).T
-                    time_series = time_series.assign(label=[fileName])
-                    for list in time_series.values.tolist():
-                        all_datasets_flattened.append(list)
-    
-    return pd.DataFrame(all_datasets_flattened)
-    
-load_dataset().to_csv("all_data_flattened.csv", index=False)
+           all_file_paths.append(f"{dirName}/{subDir}/{fileName}")
+    return all_file_paths
+
+flatten_datasets().to_csv("all_data_flattened.csv", index=False)
