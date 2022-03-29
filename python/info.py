0c9bb839aba19667f7616878b1325a14f5b6ddc1
Lasse Skaalum <lsk@trifork.com>
2022-03-29 09:54:41 +0200

    Add info module with general info regarding data

---
+++ python/info.py
@@ -0,0 +1,7 @@
+from typing import Final
+
+data_path: Final = '../datasets'
+driving_labels: Final = ['Driving']
+using_labels: Final = ['Industrial equipment', 'High vibration', 'Hand tools']
+stationary_labels: Final = ['Stationary']
+walking_labels: Final = ['Walking']
