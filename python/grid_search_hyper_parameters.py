059fb2c3a27b138ea61efb0345df63ee89fbf248
JacobToftgaardRasmussen <1jatora@gmail.com>
2022-04-06 12:58:59 +0200

    hyperparameters are set in .env

--- python/grid_search_hyper_parameters.py
+++ python/grid_search_hyper_parameters.py
@@ -1,14 +1,11 @@
+import os
 import pandas as pd
 import numpy as np
-from sklearn import svm
-from sklearn.model_selection import train_test_split
-from sklearn.model_selection import cross_val_score
-from sklearn.metrics import accuracy_score
-from sklearn.metrics import confusion_matrix
-from sklearn.metrics import ConfusionMatrixDisplay
 import modules.info as info
+from dotenv import load_dotenv
+from sklearn.metrics import accuracy_score
 from typing import Final
-from tqdm.notebook import tqdm
+from SVC import SVCConfiguration, create_and_fit_SVC_classifier, get_train_test_split
 
 # BEST RESULT SO FAR #
 # 0.875195007800312: <kernel:rbf, soft_margin:24.5, gamma:0.0001>
@@ -19,43 +16,32 @@ from tqdm.notebook import tqdm
 # 3. Soft margin (C)
 # 4. Gamma
 # 5. Time series length
-KERNELS: Final = ['rbf']
-POLY_DEGREES: Final = [2, 3, 4]
-SOFT_MARGIN: Final = np.arange(24.5, 24.6, 0.2)
-GAMMAS: Final = np.arange(0.0001, 10.0, 0.01)
+load_dotenv()
+gammas_min       = float(os.getenv('GAMMAS_MIN'))
+gammas_max       = float(os.getenv('GAMMAS_MAX'))
+gammas_step      = float(os.getenv('GAMMAS_STEP'))
 
-class SVCConfiguration:
-    def __init__(self, kernel, soft_margin, gamma, poly_degrees=2):
-        self.kernel = kernel
-        self.soft_margin = soft_margin
-        self.gamma = gamma
-        self.poly_degrees = poly_degrees
-        self.accuracy=None
+soft_margin_min  = float(os.getenv('SOFT_MARGIN_MIN'))
+soft_margin_max  = float(os.getenv('SOFT_MARGIN_MAX'))
+soft_margin_step = float(os.getenv('SOFT_MARGIN_STEP'))
 
-    def __str__(self):
-        poly_degrees = f', poly_degrees:{self.poly_degrees}' if self.kernel == 'poly' else ''
-        return f'{self.accuracy}: <kernel:{self.kernel}, soft_margin:{self.soft_margin}, gamma:{self.gamma}{poly_degrees}>'
+POLY_DEGREES     = [float(x) for x in os.getenv('POLY_DEGREES').split(",")]
 
-    def setAccuracy(self, accuracy: float):
-        self.accuracy = accuracy
-
-def get_train_test_split(df: pd.DataFrame):
-    X = df.iloc[:,:-2]
-    Y = df.iloc[:,-2:]
-    return train_test_split( X, Y, test_size=0.25)
+GAMMAS:       Final = np.arange(gammas_min, gammas_max, gammas_step)
+KERNELS:      Final = os.getenv('KERNELS').split(",")
+SOFT_MARGIN:  Final = np.arange(soft_margin_min, soft_margin_max, soft_margin_step)
 
 # Returns a precision score
 def run_svm(X_train, X_test, Y_train, Y_test, conf: SVCConfiguration) -> SVCConfiguration:
-    clf = svm.SVC(kernel=conf.kernel, C=conf.soft_margin, gamma=conf.gamma, degree=conf.poly_degrees)
-    clf.fit(X_train, Y_train.iloc[:,-1])
-    Y_pred = clf.predict(X_test)
+    classifier = create_and_fit_SVC_classifier(X_train, Y_train, conf)
+    Y_pred = classifier.predict(X_test)
     accuracy = accuracy_score(Y_test.iloc[:,-1], Y_pred)
     conf.setAccuracy(accuracy)
     return conf
 
-def main():
+def main(path_to_dataset="../datasetsModified/flattened_datasets/flattened_1sec_with_broad_category.csv"):
     # Setup
-    df = pd.read_csv("../datasetsModified/flattened_datasets/flattened_1sec_with_broad_category.csv")
+    df = pd.read_csv(path_to_dataset)
     df["broad_category"] = "None" # This adds an extra column to the df
     df = info.add_classification_to_df(df)
     X_train, X_test, Y_train, Y_test = get_train_test_split(df)
