c747fd197a6e8af571fd146fd0efacfe986ae52a
JacobToftgaardRasmussen <1jatora@gmail.com>
2022-04-06 13:19:49 +0200

    refactored flattenDataStructure to use env variables

--- python/flattenDataStructure.py
+++ python/flattenDataStructure.py
@@ -1,12 +1,16 @@
 import pandas as pd
 import math
 import numpy as np
-from os import listdir
+import os
 from typing import Final
+from dotenv import load_dotenv
 
-shortest_file_length = 768
-shortest_file_name = "Dataset air wrench 1.csv"
-ROWS_PER_TIMESERIES: Final = np.arange(1, 13, 1)
+load_dotenv()
+SOFT_MARGIN: Final = float(os.getenv('BEST_SOFT_MARGIN'))
+ROWS_PER_TIMESERIES_MIN:  Final = float(os.getenv('ROWS_PER_TIMESERIES_MIN'))
+ROWS_PER_TIMESERIES_MAX:  Final = float(os.getenv('ROWS_PER_TIMESERIES_MAX'))
+ROWS_PER_TIMESERIES_STEP: Final = float(os.getenv('ROWS_PER_TIMESERIES_STEP'))
+ROWS_PER_TIMESERIES: Final = np.arange(ROWS_PER_TIMESERIES_MIN, ROWS_PER_TIMESERIES_MAX, ROWS_PER_TIMESERIES_STEP)
 
 def create_dataframe_with_flattened_datasets(rows_per_time_series, dirName="../datasetsTU"):
     all_datasets_flattened = []
@@ -28,11 +32,15 @@ def create_time_series_partition(partition, rows_per_time_series, dataFrame, lab
 
 def create_all_file_paths(dirName="../datasetsTU"):
     all_file_paths = []
-    for subDir in listdir(dirName):
-        for fileName in listdir(f"{dirName}/{subDir}"):
+    for subDir in os.listdir(dirName):
+        for fileName in os.listdir(f"{dirName}/{subDir}"):
             all_file_paths.append(f"{dirName}/{subDir}/{fileName}")
     return all_file_paths
 
-for timeseries_size in ROWS_PER_TIMESERIES:
-    create_dataframe_with_flattened_datasets(timeseries_size).to_csv(f"all_data_noise_removed_small{timeseries_size}_points_series_flattened.csv", index=False)
-    print(timeseries_size, "done")
+def main():
+    for timeseries_size in ROWS_PER_TIMESERIES:
+        create_dataframe_with_flattened_datasets(int(timeseries_size)).to_csv(f"all_data_noise_removed_small{timeseries_size}_points_series_flattened.csv", index=False)
+        print(timeseries_size, "done")
+
+if __name__ == '__main__':
+    main()
