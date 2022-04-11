7916ae9db1ac8dd7b2794e0e412172a25ada43e8
humleflue <15886122+humleflue@users.noreply.github.com>
2022-04-11 12:20:26 +0200

    Cleanup tests and add documentation

--- c-svc/test/TransformTests.c
+++ c-svc/test/TransformTests.c
@@ -4,17 +4,63 @@
 #include <string.h>
 
 #include "CuTest/CuTest.h"
+#include "../transform.h"
 
-void AssertTrue(CuTest* tc)
+/* The name of your test should consist of three parts:
+	- The name of the method being tested.
+	- The scenario under which it's being tested.
+	- The expected behavior when the scenario is invoked.
+*/
+
+void addVectorToObservation_tryToStackSmash_shouldNotAffectStack(CuTest* tc)
 {
-	CuAssertTrue(tc, 1);
+	/* Arrange */
+	Observation o;
+	double newVector[3], arbitraryDouble = 0.42;
+	int i;
+
+	o._currentIndex = 0;
+	newVector[0] = arbitraryDouble;
+	newVector[1] = arbitraryDouble;
+	newVector[2] = arbitraryDouble;
+
+	/* Act */
+	/* We try to add one more vector (+1), than there's space for */
+	for(i = 0; i < OBSERVATION_DIM / 3 + 1; i++) {
+		addVectorToObservation(&o, newVector);
+	}
+
+	/* Assert */
+	CuAssertTrue(tc, o.observation[OBSERVATION_DIM] != arbitraryDouble);
+}
+
+void addVectorToObservation_fillUpObservations_shouldFillUpLastIndex(CuTest* tc)
+{
+	/* Arrange */
+	Observation o;
+	double newVector[3], arbitraryDouble = 0.42;
+	int i;
+
+	o._currentIndex = 0;
+	newVector[0] = arbitraryDouble;
+	newVector[1] = arbitraryDouble;
+	newVector[2] = arbitraryDouble;
+
+	/* Act */
+	for(i = 0; i < OBSERVATION_DIM / 3; i++) {
+		addVectorToObservation(&o, newVector);
+	}
+
+	/* Assert */
+	CuAssertTrue(tc, o.observation[OBSERVATION_DIM - 1] == arbitraryDouble);
 }
 
 CuSuite* CuGetSuite(void)
 {
 	CuSuite* suite = CuSuiteNew();
 
-	SUITE_ADD_TEST(suite, AssertTrue);
+	SUITE_ADD_TEST(suite, addVectorToObservation_tryToStackSmash_shouldNotAffectStack);
+	SUITE_ADD_TEST(suite, addVectorToObservation_fillUpObservations_shouldFillUpLastIndex);
 
 	return suite;
 }
--- c-svc/transform.c
+++ c-svc/transform.c
@@ -1,17 +1,28 @@
 #include "transform.h"
 
-void transform(Observation *o, double newVector[3]) {
+void addVectorToObservation(Observation *o, double newVector[3]) {
     int i = o->_currentIndex;
     
-    assert(!o->isFull);
-    
-    o->observation[i  ] = newVector[0];
-    o->observation[i+1] = newVector[1];
-    o->observation[i+2] = newVector[2];
-    
-    o->_currentIndex += 3;
+    if(!o->isFull) {
+        o->observation[i  ] = newVector[0];
+        o->observation[i+1] = newVector[1];
+        o->observation[i+2] = newVector[2];
+        
+        o->_currentIndex += 3;
 
-    if(o->_currentIndex == OBSERVATION_DIM) {
-        o->isFull = true;
+        if(o->_currentIndex == OBSERVATION_DIM) {
+            o->isFull = true;
+        }
     }
+}
+
+/* JUST A MOCKED SHELL */
+TransformedObservation transform(Observation o) {
+    TransformedObservation res;
+
+    res.observation[0] = o.observation[0];
+    res.observation[1] = o.observation[1];
+    res.observation[2] = o.observation[2];
+
+    return res;
 }
