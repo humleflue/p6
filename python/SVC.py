c6dd2cfbb62130dc2dd914839d9aca90007221ec
laurits-mumberg <laurits.mumberg@gmail.com>
2022-04-19 09:46:31 +0200

    Moved the functions out of main

--- python/SVC.py
+++ python/SVC.py
@@ -2,6 +2,7 @@ import os
 import pandas as pd
 from dotenv import load_dotenv
 from sklearn import svm
+import math
 from sklearn.model_selection import train_test_split
 
 class SVCConfiguration:
@@ -26,6 +27,7 @@ class SVCConfiguration:
 def create_and_fit_SVC_classifier(X_train, Y_train, conf: SVCConfiguration) -> SVCConfiguration:
     classifier = svm.SVC(kernel=conf.kernel, C=conf.soft_margin, gamma=conf.gamma, degree=conf.poly_degrees)
     classifier.fit(X_train, Y_train.iloc[:,-1])
+
     return classifier
 
 def get_default_config() -> SVCConfiguration:
@@ -39,3 +41,38 @@ def get_train_test_split(df: pd.DataFrame):
     X = df.iloc[:,:-2]
     Y = df.iloc[:,-2:]
     return train_test_split( X, Y, test_size=0.25)
+
+# Returns a dataframe equal in structure to the flattened dataframe, but only with "percentage" amount of columns.
+def sample_flattened_dataset(df, percentage):
+    df_last_two_columns = df.iloc[:,-2:]
+    df_without_last_two_columns = df.iloc[:,:-2]
+    df_with_sampling = df_without_last_two_columns.sample(frac = percentage, axis = 1) # Axis = 1 means columns. 0 is rows
+    res = pd.merge(df_with_sampling, df_last_two_columns, left_index=True, right_index=True) # Combines the two dataframes on index
+    
+    return res
+
+# TODO: This is not acutally the magnitude. Find out what it is, or if something else should be used
+def flattened_dataframe_avg_magnitude(df):
+    # Seperating the data labels from the actual data
+    df_last_two_columns = df.iloc[:,-2:]
+    df_without_last_two_columns = df.iloc[:,:-2]
+    
+    # New dataframe that will contain the magnitude data
+    new_df = pd.DataFrame(columns = ["x","y","z"])
+
+    # Loops through all rows (This is bad practice)
+    for index1, row in df_without_last_two_columns.iterrows():
+        # List containing the sum of x, y, and z squared and summed.
+        time_series_sum = [0,0,0]
+
+        # Loops through all data points in a row and adds the squared value to time_series_sum
+        for index2, ele in enumerate(row.to_list()):
+            time_series_sum[index2 % 3] += (ele ** 2)
+
+        # Takes the square root of the sum, and appends it to the new dataframe
+        time_series_avg = [math.sqrt(x) for x in time_series_sum]
+        new_df.loc[index1] = time_series_avg
+    
+    # Combines the two dataframes on index, thereby adding the label back
+    res = pd.merge(new_df, df_last_two_columns, left_index=True, right_index=True) 
+    return res
--- python/main.py
+++ python/main.py
@@ -1,8 +1,7 @@
 import os
-import math
 import pandas as pd
 from sklearn.metrics import accuracy_score, classification_report
-from SVC import create_and_fit_SVC_classifier, get_default_config, get_train_test_split
+from SVC import create_and_fit_SVC_classifier, get_default_config, get_train_test_split, flattened_dataframe_avg_magnitude
 from dotenv import load_dotenv
 
 def main():
@@ -11,9 +10,9 @@ def main():
     # Load data
     dataset_path = os.getenv('BEST_DATASET')
     df = pd.read_csv(dataset_path)
-    df_sampled_data = sample_flattened_dataset(df)
-    df_avg_data =  average_sampling(df)
-    X_train, X_test, Y_train, Y_test = get_train_test_split(df_avg_data)
+    mag_data =  flattened_dataframe_avg_magnitude(df)
+    print(mag_data)
+    X_train, X_test, Y_train, Y_test = get_train_test_split(mag_data)
 
     # Create and fit model   
     model_config = get_default_config()
@@ -25,45 +24,10 @@ def main():
     model_config.set_accuracy(accuracy)
     report = classification_report(Y_test.iloc[:,-1], Y_pred)
     model_config.set_report(report)
-    
+
+    print(classifier.coef_)
+
     print(model_config)
-
-def sample_flattened_dataset(df):
-    df_last_two_columns = df.iloc[:,-2:]
-    df_without_last_two_columns = df.iloc[:,:-2]
-    df_with_sampling = df_without_last_two_columns.sample(frac = 0.25, axis = 1) # Axis = 1 means columns. 0 is rows
-    res = pd.merge(df_with_sampling, df_last_two_columns, left_index=True, right_index=True) # Combines the two dataframes on index
-    
-    return res
-
-def average_sampling(df):
-    # Seperating the data labels from the actual data
-    df_last_two_columns = df.iloc[:,-2:]
-    df_without_last_two_columns = df.iloc[:,:-2]
-    
-    # New dataframe that will contain mean square data
-    new_df = pd.DataFrame(columns = ["x","y","z"])
-
-    # Loops through all rows
-    for index1, row in df_without_last_two_columns.iterrows():
-        # List containing the sum of x, y, and z squared.
-        time_series_sum = [0,0,0]
-
-        # Loops through all data points in a row and adds the squared value to time_series_sum
-        for index2, ele in enumerate(row.to_list()):
-            time_series_sum[index2 % 3] += (ele ** 2)
-
-        #length_of_row = row.shape[0]
-
-        # Takes the square root of the sum, and appends it to the new dataframe
-        time_series_avg = [math.sqrt(x) for x in time_series_sum]
-        new_df.loc[index1] = time_series_avg
-    
-    # Combines the two dataframes on index, thereby adding the label back
-    res = pd.merge(new_df, df_last_two_columns, left_index=True, right_index=True) 
-    print(res)
-    return res
-
 
 
 if __name__ == '__main__':
